75888e4712526edfde82629224000bed0678c715
GlebMozglyakov <mozglgleb2004@gmail.com>
2023-02-23 19:12:17 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using QueueOnTwoStacks;
+
+Console.WriteLine("Hello, World!");
+
+var queue = new QueueTwoStacks<int>();
+queue.Push(1);
+queue.Push(2);
+queue.Push(3);
+queue.Push(44);
+Console.WriteLine(queue.Pop());
+Console.WriteLine(queue.Pop());
+Console.WriteLine(queue.Pop());
+Console.WriteLine(queue.Pop());
---
+++ QueueTwoStacks.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueOnTwoStacks
+{
+    public class QueueTwoStacks<T>
+    {
+        private Stack<T> StackForPush { get; set; }
+
+        private Stack<T> ReverseStack { get; set; }
+
+        public QueueTwoStacks()
+        {
+            StackForPush = new Stack<T>();
+            ReverseStack = new Stack<T>();
+        }
+
+        public void Push(T QueueItem)
+        {
+            StackForPush.Push(QueueItem);
+        }
+
+        public T Pop()
+        {
+            if (ReverseStack.Count == 0)
+            {
+                while (StackForPush.Count > 0)
+                    ReverseStack.Push(StackForPush.Pop());
+            }
+            if (ReverseStack.Count > 0)
+                return ReverseStack.Pop();
+            else
+                throw new NullReferenceException("Очередь пуста !");
+        }
+    }
+}
